fa19e600e1116af1a2bcf6c35cfd07658e7ca9a0
@soyjavi <javi.jimenez.villar@gmail.com>
2018-11-13 19:42:51 +0700

    Lint code

--- src/services/transactions.js
+++ src/services/transactions.js
@@ -5,7 +5,9 @@ import { C } from '../common';
 const { BLOCKCHAIN_TXS } = C;
 
 export default ({ props: { year = new Date().getFullYear().toString() }, session }, res) => {
-  const { blocks: [, ...txs] } = new Blockchain({ ...BLOCKCHAIN_TXS, file: session.hash, key: year, readMode: true });
+  const { blocks: [, ...txs] } = new Blockchain({
+    ...BLOCKCHAIN_TXS, file: session.hash, key: year, readMode: true,
+  });
 
   return res.json({
     txs: txs.map(({ hash, timestamp, data }) => ({ hash, timestamp: data.timestamp || timestamp, ...data })),
