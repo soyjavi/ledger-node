6a7f9a8c0e720c1370b3c399eb1da66fbc9e27d6
@soyjavi <javi.jimenez.villar@gmail.com>
2018-11-15 19:55:41 +0700

    Rename baseCurrency

--- src/services/profile.js
+++ src/services/profile.js
@@ -9,17 +9,17 @@ export default async ({ session }, res) => {
   const year = new Date().getFullYear().toString();
 
   const { blocks: [, ...vaults] } = new Blockchain({ ...BLOCKCHAIN_VAULTS, file: session.hash });
-  const currency = vaults[0] ? vaults[0].data.currency : CURRENCY;
+  const baseCurrency = vaults[0] ? vaults[0].data.currency : CURRENCY;
 
   const { latestBlock } = new Blockchain({ ...BLOCKCHAIN_TXS, file: session.hash, key: year });
 
   return res.json({
-    currency,
+    baseCurrency,
     latestTransaction: {
       hash: latestBlock.hash,
       timestamp: latestBlock.timestamp,
     },
-    rates: await rates(currency),
+    rates: await rates(baseCurrency),
     vaults: vaults.map(({ data, hash }) => ({ hash, ...data })),
   });
 };
