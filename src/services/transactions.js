b088ec7ad42c29d5aea1ef8964e9ca32b173da28
@soyjavi <javi.jimenez.villar@gmail.com>
2018-12-06 13:53:05 +0700

    New parameter for filter transactions using the latest transaction

--- src/services/transactions.js
+++ src/services/transactions.js
@@ -1,13 +1,25 @@
 import Blockchain from 'vanillachain-core';
 
-import { C } from '../common';
+import { C, ERROR } from '../common';
 
 const { BLOCKCHAIN } = C;
 
-export default ({ props: { year = new Date().getFullYear().toString() }, session }, res) => {
-  const { blocks: [, ...txs] } = new Blockchain({
+export default ({ props, session }, res) => {
+  const {
+    latestTransaction,
+    year = new Date().getFullYear().toString(),
+  } = props;
+
+  let { blocks: [, ...txs] } = new Blockchain({
     ...BLOCKCHAIN, file: session.hash, key: year, readMode: true,
   });
+
+  if (latestTransaction) {
+    const index = txs.findIndex(({ hash }) => hash === latestTransaction);
+
+    if (index < 0) return ERROR.NOT_FOUND(res);
+    txs = txs.slice(index + 1, txs.length);
+  }
 
   return res.json({
     txs: txs.map(({ hash, timestamp, data }) => ({ hash, timestamp: data.timestamp || timestamp, ...data })),
