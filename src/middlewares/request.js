7b12cc89dcc2ca69eb9397b8758ffff6b06bed41
@soyjavi <javi.jimenez.villar@gmail.com>
2019-01-01 08:36:25 +0700

    Fixed typo

--- src/middlewares/request.js
+++ src/middlewares/request.js
@@ -4,13 +4,12 @@ import Blockchain from 'vanillachain-core';
 import { cache, C, ERROR } from '../common';
 import MAP from '../../map.json';
 
-const { BLOCKCHAIN } = C;
+const { BLOCKCHAIN, KEY } = C;
 
 export default (req, res, next) => {
   const { originalUrl } = req;
   const today = new Date();
   const timestamp = today.getTime();
-  const year = today.getFullYear().toString();
 
   const route = MAP[originalUrl.split('/')[1].split('?')[0]]; // eslint-disable-line
   if (!route) return ERROR.UNKNOWN_SERVICE(res);
@@ -26,7 +25,7 @@ export default (req, res, next) => {
     let session = cache.get(authorization);
     if (!session) {
       const { blocks: [, ...vaults] } = new Blockchain({ ...BLOCKCHAIN, file: authorization, key: 'vaults' });
-      const { blocks: [, lastTX] } = new Blockchain({ ...BLOCKCHAIN, file: authorization, key: year });
+      const { blocks: [, lastTX] } = new Blockchain({ ...BLOCKCHAIN, file: authorization, key: KEY });
 
       session = {
         hash: authorization,
