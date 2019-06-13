4c3e3906dc7468a6c11a0834e012550848f0c680
@soyjavi <javi.jimenez.villar@gmail.com>
2019-06-13 16:49:11 +0700

    Show cache status in /status endpoint

--- src/services/status.js
+++ src/services/status.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 
+import { cache } from '../common';
 import PKG from '../../package.json';
 
 dotenv.config();
@@ -9,6 +10,7 @@ export default (req, res, next) => {
   res.dataSource = {
     instance: INSTANCE,
     version: PKG.version,
+    cache: cache.status,
   };
 
   next();
