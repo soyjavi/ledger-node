8befd87b5f2f13ff197ebbddfe909a39c6aafcc8
@soyjavi <hello@soyjavi.com>
2019-06-21 14:50:11 +0700

    Update status services

--- src/services/status.js
+++ src/services/status.js
@@ -1,16 +1,21 @@
+import Blockchain from 'vanillachain-core';
 import dotenv from 'dotenv';
 
-import { cache } from '../common';
+import { cache, C } from '../common';
 import PKG from '../../package.json';
 
 dotenv.config();
 const { INSTANCE } = process.env;
+const { BLOCKCHAIN } = C;
 
 export default (req, res, next) => {
+  const blockchain = new Blockchain(BLOCKCHAIN);
+
   res.dataSource = {
     instance: INSTANCE,
     version: PKG.version,
-    cache: cache.status,
+    cache: cache.status.bytes,
+    sessions: blockchain.blocks.length,
   };
 
   next();
