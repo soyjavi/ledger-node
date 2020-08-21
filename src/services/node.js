53865e80e3f51902235762ba53faa41fae1a4abf
JΛVI <hello@soyjavi.com>
2020-08-21 21:03:20 +0700

    Improved node service

--- src/services/node.js
+++ src/services/node.js
@@ -1,9 +1,17 @@
+import fs from "fs";
+import path from "path";
+
 import { Blockchain } from "vanilla-blockchain";
 import { Storage } from "vanilla-storage";
 
 import { C, ERROR } from "../common";
 
 const { BLOCKCHAIN, STORAGE, KEY_VAULTS, KEY_TRANSACTIONS } = C;
+
+const latestHash = (storage, key) => {
+  const { value = [] } = storage.get(key);
+  return value.length > 0 ? value.slice(-1).pop().hash : undefined;
+};
 
 // -----------------------------------------------------------------------------
 // SIGNUP
@@ -29,16 +37,11 @@ export const state = ({ session }, res) => {
 
   const blocks = (key) => storage.get(key).value.length;
 
-  const latestHash = (key) => {
-    const { value = [] } = storage.get(key);
-    return value.length > 0 ? value.slice(-1).pop().hash : undefined;
-  };
-
   res.json({
     blocks: { txs: blocks(KEY_TRANSACTIONS), vaults: blocks(KEY_VAULTS) },
     latestHash: {
-      txs: latestHash(KEY_TRANSACTIONS),
-      vaults: latestHash(KEY_VAULTS),
+      txs: latestHash(storage, KEY_TRANSACTIONS),
+      vaults: latestHash(storage, KEY_VAULTS),
     },
   });
 };
@@ -50,24 +53,31 @@ export const sync = (
   { props: { blockchain, block, blocks = [], key }, session },
   res
 ) => {
-  const storage = new Storage({ ...STORAGE, ...session });
   let response;
 
   if (blockchain) {
-    const { vaults, txs } = blockchain;
+    const filePath = path.resolve(".", `store/${session.filename}.json`);
+    if (fs.existsSync(filePath)) fs.unlinkSync(filePath);
 
-    storage.wipe();
-    storage.get("vaults").save(vaults);
-    storage.get("txs").save(txs);
-
-    response = blockchain;
+    const storage = new Storage({ ...STORAGE, ...session });
+    Object.keys(blockchain).forEach((key) => {
+      storage.get(key);
+      blockchain[key].forEach((block) => storage.push(block));
+    });
   } else if (key) {
+    const storage = new Storage({ ...STORAGE, ...session });
     storage.get(key);
-    if (block) response = storage.push(block);
-    else if (blocks) response = blocks.map((block) => storage.push(block));
+
+    const { previousHash } = block || blocks[0] || {};
+    const hash = latestHash(storage, key);
+
+    if (hash !== previousHash) return ERROR.NOT_FOUND(res);
+
+    if (block) storage.push(block);
+    else if (blocks) blocks.map((block) => storage.push(block));
   }
 
-  res.json(response);
+  state({ session }, res);
 };
 
 // -----------------------------------------------------------------------------
